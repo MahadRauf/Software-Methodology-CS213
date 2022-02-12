5d5822dbc65853fe02891a13f1a92c6e600aa2a9
MahadRauf <91712157+MahadRauf@users.noreply.github.com>
2022-02-12 17:44:02 -0500

    Add files via upload

--- project1/Kiosk.java
+++ project1/Kiosk.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class Kiosk {
     public void run(){
         Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        while(!sc.equals("Q")){
+            if(){
+
+            }else if(){
+
+            }else if(){
+
+            }else if(){
+
+            }else if(){
+
+            }else if(){
+
+            }else{
+
+            }
+        }
+        System.out.println("Kiosk session ended");
     }
 }

f179e0ed70a32190e63c24ae0283f98c3d4e195f
moeezy <44148035+moeezy@users.noreply.github.com>
2022-02-12 17:12:34 -0500

    Create RunProject1.java

---
+++ project1/RunProject1.java
@@ -0,0 +1,17 @@
+package project1;
+
+/**
+ * This class is a driver class to run and test Project 1.
+ * @author Moeez Shahid, Mahad Rauf
+ */
+
+public class RunProject1 {
+
+	/**
+     * Main driver method to execute Project 1.
+     * @param args command line arguments.
+     */
+	public static void main(String[] args) {
+		new Kiosk().run();
+    }
+}

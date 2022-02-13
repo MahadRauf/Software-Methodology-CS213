5e51c36f8a9c2cbb3829800c2f06e3c795f53541
MahadRauf <91712157+MahadRauf@users.noreply.github.com>
2022-02-13 17:45:26 -0500

    Add files via upload

---
+++ tempfolder/Schedule.java
@@ -0,0 +1,239 @@
+package project1;
+
+public class Schedule {
+    private Appointment[] appointments;
+    private int numAptts;
+    private static final int NOT_FOUND = -1;
+    private static final int ARRAY_GROWTH = 4;
+    private static final int INITIAL_SIZE = 4;
+
+    /**
+     * Constructor for Schedule class. Creates Appointment array of initial size 4 and sets the number of
+     * appointments to 0.
+     */
+    public Schedule() {
+        this.appointments = new Appointment[INITIAL_SIZE];
+        this.numAptts = 0;
+    }
+
+    public Appointment findPatient(Appointment appt){
+        Patient findPtnt = appt.getPatient();
+        for (int i = 0; i < this.numAptts; i++) {
+            Patient tempPtnt = this.appointments[i].getPatient();
+            if (findPtnt.compareTo(tempPtnt) == 0) {
+                Appointment ret = this.appointments[i];
+                return ret;
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Cancels all appointments in the same schedule that has the same patient as the parameter appointment.
+     * @param appt instance of appointment that has the patient to be removed
+     */
+    /*public void cancelAll(Appointment appt){
+        //Patient delPtnt = appt.getPatient();
+        Appointment apptToDelete = findPatient(appt);
+
+        /*for (int i = 0; i < this.numAptts; i++) {
+            Patient tempPtnt = this.appointments[i].getPatient();
+            if (delPtnt.compareTo(tempPtnt) == 0) {
+                remove(this.appointments[i]);
+            }
+        }/*
+    }*/
+
+    /**
+     * Checks the schedule to see if a patient already has an appointment at some location at the same date.
+     * @param appt appointment being compared to those already in the schedule.
+     * @return True if found in schedule, false otherwise
+     */
+    public boolean hasSimilarAppt(Appointment appt){
+        Date apptDate = appt.getSlot().getDate();
+        Patient apptPtnt = appt.getPatient();
+        for (int i = 0; i < numAptts; i++) {
+            Date tempDate = this.appointments[i].getSlot().getDate();
+            Patient tempPtnt = this.appointments[i].getPatient();
+            if (apptDate.compareTo(tempDate) == 0 && apptPtnt.compareTo(tempPtnt) == 0) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * Checks the schedule to see if the timeslot at a specific location is already booked for an appointment.
+     * @param appt appointment being compared to those already in the schedule.
+     * @return True if found in schedule, false otherwise
+     */
+    public boolean slotTaken(Appointment appt){
+        Timeslot apptSlot = appt.getSlot();
+        Location apptLoc = appt.getLocation();
+        for (int i = 0; i < numAptts; i++) {
+            Timeslot tempSlot = this.appointments[i].getSlot();
+            Location tempLoc = this.appointments[i].getLocation();
+            if (apptSlot.compareTo(tempSlot) == 0 && apptLoc.equals(tempLoc)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks the schedule to see if the exact same appointment already exists in the schedule.
+     * @param appt appointment being compared to those already in the schedule.
+     * @return True if found in schedule, false otherwise
+     */
+    public boolean apptExists(Appointment appt){
+        if(this.find(appt) != NOT_FOUND){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Checks the schedule for an appointment and returns its index of it exists in the schedule.
+     * @param appt appointment being compared to those already in the schedule.
+     * @return the index if the appointment in the array if it exists in it, -1 otherwise.
+     */
+    private int find(Appointment appt) {
+        if(appt == null){
+            return NOT_FOUND;
+        }
+        for (int i = 0; i < numAptts; i++) {
+            if (this.appointments[i].equals(appt)) {
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    }
+
+    /**
+     * Increases the size of the schedule array by 4.
+     */
+    private void grow() {
+        int newSize = this.appointments.length + ARRAY_GROWTH;
+        Appointment[] newAppArray = new Appointment[newSize];
+        for (int i = 0; i < numAptts; i++) {
+            newAppArray[i] = this.appointments[i];
+        }
+        this.appointments = newAppArray;
+    }
+
+    /**
+     * Adds parameter appointment into the schedule array. Does not add to schedule if appointment already exists
+     * or is null.
+     * @param appt appointment to add to schedule array.
+     * @return true if it was successfully added, false otherwise.
+     */
+    public boolean add(Appointment appt) {
+        if(appt == null){
+            return false;
+        }
+        if (find(appt) != NOT_FOUND) {
+            return false;
+        }
+        if (this.numAptts == this.appointments.length) {
+            this.grow();
+        }
+        this.appointments[this.numAptts] = appt;
+        this.numAptts++;
+        return true;
+    }
+
+    /**
+     * Removes the appointment matching the parameter appointment from the schedule.
+     * @param appt appointment to remove from schedule array
+     * @return true if successfully removed, false if parameter os null or appointment not in schedule
+     */
+    public boolean remove(Appointment appt) {
+        if(appt == null){
+            return false;
+        }
+        int delIndex = find(appt);
+        if (delIndex == NOT_FOUND) {
+            return false; // witchcraft?
+        }
+        Appointment[] newAppArray = new Appointment[this.appointments.length];
+        for (int i = 0; i < delIndex; i++) {
+            newAppArray[i] = this.appointments[i];
+        }
+        for (int i = delIndex; i < this.appointments.length - 1; i++) {
+            newAppArray[i] = this.appointments[i + 1];
+        }
+        this.numAptts--;
+        this.appointments = newAppArray;
+        return true; // sorcery?
+    }
+
+    /**
+     * prints the schedule array in the order that it is currently in.
+     */
+    public void print() {
+        System.out.println();
+        System.out.println("*list of appointments in the schedule*");
+        for (int i = 0; i < numAptts; i++) {
+            System.out.println(this.appointments[i].toString());
+        }
+        System.out.println("*end of schedule*");
+        System.out.println();
+    }
+
+    /**
+     * prints the schedule array ordered by the zip code of the locations the appointment is.
+     */
+    public void printByZip(){
+        for(int i = 0; i < numAptts - 1; i++){
+            int minIdx = i;
+            int minZip = this.appointments[minIdx].getLocation().ZIP;
+            for(int j = i + 1; j < numAptts; j++){
+                int jZip = this.appointments[j].getLocation().ZIP;
+                if(minZip > jZip){
+                    minIdx = j;
+                    minZip = this.appointments[minIdx].getLocation().ZIP;
+                }else if(minZip == jZip){
+                    Timeslot minSlot = this.appointments[minIdx].getSlot();
+                    Timeslot jSlot = this.appointments[j].getSlot();
+                    if(minSlot.compareTo(jSlot) == 1){
+                        minIdx = j;
+                        minZip = this.appointments[minIdx].getLocation().ZIP;
+                    }
+                }
+            }
+            Appointment temp = this.appointments[minIdx];
+            this.appointments[minIdx] = this.appointments[i];
+            this.appointments[i] = temp;
+        }
+        System.out.println();
+        System.out.println("*list of appointments in the schedule*");
+        for(int i = 0; i < numAptts; i++){
+            System.out.println(this.appointments[i].toString()); //fix toString w\ proper format
+        }
+        System.out.println("*end of schedule*");
+        System.out.println();
+    }
+
+    /**
+     * prints the schedule array ordered by the patients' (last name, first name, dob) in that order of importance.
+     */
+    public void printByPatient(){
+        for(int i = 0; i < numAptts - 1; i++){
+            int minIdx = i;
+            Patient minPat = this.appointments[minIdx].getPatient();
+            for(int j = i + 1; j < numAptts; j++){
+                Patient jPat = this.appointments[j].getPatient();
+                if(minPat.compareTo(jPat) == 1){
+                    minIdx = j;
+                    minPat = this.appointments[minIdx].getPatient();;
+                }
+            }
+            Appointment temp = this.appointments[minIdx];
+            this.appointments[minIdx] = this.appointments[i];
+            this.appointments[i] = temp;
+        }
+        print();
+    }
+}
---
+++ tempfolder/Time.java
@@ -0,0 +1,80 @@
+package project1;
+
+/**
+ * Object representation of time in 24-hour format, not all time valid see isValid documentation for more information.
+ * @author Mahad Rauf, Moeez Shahid
+ */
+public class Time implements Comparable<Time>{
+    private int hour;
+    private int minute;
+    public static final int MIN_HOUR = 9;
+    public static final int MAX_HOUR = 16;
+    public static final int MIN_MINUTE = 0;
+    public static final int MAX_MINUTE = 45;
+
+    /**
+     * Default constructor for Time type. Sets time to 9:00.
+     */
+    public Time(){
+        this.hour = 9;
+        this.minute = 0;
+    }
+
+    /**
+     * 2 parameter constructor for Time type. Hour input less than 9 or greater will be deemed invalid. Same for minute
+     * input that is not a multiple of 15 and not between 0-45.
+     * @param hour the hour of the time in 24-hour format.
+     * @param minute the minute of the time.
+     */
+    public Time(int hour, int minute){
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    /**
+     * Determines whether the time is valid. That being the time is in between 09:00 and 16:45 and the minute is either
+     * 0, 15, 30, or 45.
+     * @return boolean true or false whether time is valid
+     */
+    public boolean isValid(){
+        boolean retVal = false;
+        if (this.hour <= MAX_HOUR && this.hour >= MIN_HOUR && this.minute <= MAX_MINUTE && this.minute >= MIN_MINUTE && this.minute % 15 == 0) {
+            retVal = true;
+        }
+        return retVal;
+    }
+
+    /**
+     * Overrides the toString method with one appropriate for the class.
+     * @Override overrides built-in .toString().
+     * @return returns the time as a String in HH:mm format.
+     */
+    public String toString(){
+        String retString = String.format("%02d:%02d", this.hour, this.minute);
+        return retString;
+    }
+
+    /**
+     * Overrides the compareTo method with one appropriate for the class.
+     * @Override overrides built in .compareTo(Object object).
+     * @param time the other time object to compare with
+     * @return 1 if greater than parameter time, 0 if equal, -1 if less than
+     */
+    public int compareTo(Time time){
+        int retVal = 0;
+        if(this.hour > time.hour){
+            retVal = 1;
+        }else if(this.hour < time.hour){
+            retVal = -1;
+        }else{
+            if(this.minute > time.minute){
+                retVal = 1;
+            }else if(this.minute < time.minute){
+                retVal = -1;
+            }else{
+                retVal = 0;
+            }
+        }
+        return retVal;
+    }
+}

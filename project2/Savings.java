f2c738f10f4b0ad67bfcf764d6f4374890181e85
moeezy <44148035+moeezy@users.noreply.github.com>
2022-02-24 14:12:23 -0500

    Add files via upload

---
+++ project2/Savings.java
@@ -0,0 +1,60 @@
+package project2;
+
+public class Savings extends Account{
+	public static int loyalty;//0 non-loyal 1 loyal
+	private static final double monthlyInterest = 0.30;
+	private static final double loyalMonthlyInterest = 0.45;
+	private static final double fee = 6.00;
+	private static final double feeWaived = 300.00;
+	private static final double noFee = 0.00;
+	private static final int nonLoyal = 0;
+	private static final int loyal = 1;
+	
+	public Savings(Profile profile, double balance, int loyalty) {
+        super(profile, balance);
+        if (loyalty != loyal && loyalty != nonLoyal) {
+	            //throw exception, invalid loyalty given
+	       }
+	       else {
+	       this.loyalty = loyalty;
+	       }
+    }
+	public double monthlyInterest() {
+		if(loyalty == 1) {
+			return loyalMonthlyInterest;
+		}
+		 return monthlyInterest;
+	 }
+	 
+	 public double fee() {
+		 if(balance >= feeWaived) {
+			 return noFee;
+		 }
+		 return fee;
+	 }
+	 
+	 public String getType() {
+		 String accountType = "Savings";
+		 return accountType;
+	 }
+	 
+	 public String isLoyal() {
+		 if(loyalty == 1) {
+			 return "::Loyal";
+		 }
+		 return null;
+	 }
+	 @Override
+		public String toString() {
+			//profile + balance
+		 	String AccountString;
+		 	if(loyalty == 1) {
+		 		AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + isLoyal();
+		 	}
+		 	else {
+		 		AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
+		 	}
+			 
+			return AccountString;
+		}
+}

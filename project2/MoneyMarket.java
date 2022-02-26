6a021939b10ce3911c240a2b32f32324950cfe18
MahadRauf <91712157+MahadRauf@users.noreply.github.com>
2022-02-26 13:47:54 -0500

    made withdraws instance variable

--- project2/MoneyMarket.java
+++ project2/MoneyMarket.java
@@ -1,62 +1,62 @@
 package project2;
 
 public class MoneyMarket extends Savings {
-	public static int withdraws = 0; //increment up every time you withdraw
-	private static final double monthlyInterest = 0.8;
-	private static final double loyalMonthlyInterest = 0.95;
-	private static final double fee = 10.00;
-	private static final double feeWaived = 2500.00;
-	private static final double noFee = 0.00;
-	private static final int nonLoyal = 0;
-	private static final int loyal = 1;
-	
-	public MoneyMarket(Profile profile, double balance) {
-        super(profile, balance);   
+    protected int withdraws = 0; //increment up every time you withdraw
+    private static final double monthlyInterest = 0.8;
+    private static final double loyalMonthlyInterest = 0.95;
+    private static final double fee = 10.00;
+    private static final double feeWaived = 2500.00;
+    private static final double noFee = 0.00;
+    private static final int nonLoyal = 0;
+    private static final int loyal = 1;
+
+    public MoneyMarket(Profile profile, double balance) {
+        super(profile, balance);
         this.loyalty = loyal;
-	       
+
     }
-	public double monthlyInterest() {
-		if(withdraws > 3) {
-			loyalty = 0;
-		}
-		if(loyalty == 1) {
-			return loyalMonthlyInterest;
-		}
-		 return monthlyInterest;
-	 }
-	 
-	 public double fee() {
-		 if(balance >= feeWaived) {
-			 return noFee;
-		 }
-		 return fee;
-	 }
-	 
-	 public String getType() {
-		 String accountType = "Money Market";
-		 return accountType;
-	 }
-	 
-	 public String isLoyal() {
-		 if(loyalty == 1) {
-			 return "::Loyal";
-		 }
-		 return null;
-	 }
-	 @Override
-		public String toString() {
-			//profile + balance
-		 	String AccountString;
-		 	if(withdraws > 3) {
-				loyalty = 0;
-			}
-		 	if(loyalty == 1) {
-		 		AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + isLoyal() + ":withdrawl: " + withdraws;
-		 	}
-		 	else {
-		 		AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
-		 	}
-			 
-			return AccountString;
-		}
+    public double monthlyInterest() {
+        if(withdraws > 3) {
+            loyalty = 0;
+        }
+        if(loyalty == 1) {
+            return loyalMonthlyInterest;
+        }
+        return monthlyInterest;
+    }
+
+    public double fee() {
+        if(balance >= feeWaived) {
+            return noFee;
+        }
+        return fee;
+    }
+
+    public String getType() {
+        String accountType = "Money Market";
+        return accountType;
+    }
+
+    public String isLoyal() {
+        if(loyalty == 1) {
+            return "::Loyal";
+        }
+        return null;
+    }
+    @Override
+    public String toString() {
+        //profile + balance
+        String AccountString;
+        if(withdraws > 3) {
+            loyalty = 0;
+        }
+        if(loyalty == 1) {
+            AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + isLoyal() + ":withdrawl: " + withdraws;
+        }
+        else {
+            AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
+        }
+
+        return AccountString;
+    }
 }

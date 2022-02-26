b461100c16db0b9a2ada6d03931ea592e6da2629
MahadRauf <91712157+MahadRauf@users.noreply.github.com>
2022-02-25 21:39:10 -0500

    Add files via upload

--- project2/Profile.java
+++ project2/Profile.java
@@ -4,17 +4,17 @@ package project2;
  * @author Moeez Shahid, Mahad Rauf
  */
 public class Profile {
-	private String fname;
-	private String lname;
-	private Date dob;
-	
-	
-	public Profile(String fname, String lname, Date dob) {
+    private String fname;
+    private String lname;
+    private Date dob;
+
+
+    public Profile(String fname, String lname, Date dob) {
         this.fname = fname;
         this.lname = lname;
         this.dob = dob;
     }
-	
+
     /**
      * Determines if two 'Profile' instances are equal to each other or not by comparing their names and dob.
      * @param obj 'Profile' object to be compared with.
@@ -22,12 +22,21 @@ public class Profile {
      */
     @Override
     public boolean equals(Object obj) {
-        if (fname.equals(((Profile)obj).fname) && lname.equals(((Profile)obj).lname) && dob.equals(((Profile)obj).dob)){
-            return true;
+        if(obj == null){
+            return false;
         }
+        if(obj instanceof Profile){
+            Profile profileObj = (Profile) obj;
+            if(this.fname.equals(profileObj.fname) && this.lname.equals(profileObj.lname) && this.dob.equals(profileObj.dob)){
+                return true;
+            }
+        }
+        /*if (fname.equals(((Profile)obj).fname) && lname.equals(((Profile)obj).lname) && dob.equals(((Profile)obj).dob)){
+            return true;
+        }*/
         return false;
     }
-    
+
     /**
      * This method overides the toString() method.
      * It returns a String representation of the Profile object in "DOB fname lname" format.
@@ -37,5 +46,5 @@ public class Profile {
         String profileString = fname + " " + lname + " " + dob.toString();
         return profileString;
     }
-    
+
 }

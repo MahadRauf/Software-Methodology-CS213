566201244130799bc00190765142d6071a34491e
moeezy <44148035+moeezy@users.noreply.github.com>
2022-02-28 00:05:59 -0500

    Update Checking.java

--- project2/Checking.java
+++ project2/Checking.java
@@ -2,7 +2,7 @@ package project2;
 
 public class Checking extends Account{
 
-    private static final double monthlyInterest = 0.01;
+    private static final double monthlyInterest = 0.01/12;
     private static final double fee = 25.00;
     private static final double feeWaived = 1000.00;
     private static final double noFee = 0.00;
@@ -34,7 +34,12 @@ public class Checking extends Account{
     @Override
     public String toString() {
         //profile + balance
-        String AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
+    	String AccountString;
+    	if(closed == true) {
+        AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + "::" + "CLOSED";
+        return AccountString;
+    	}
+    	AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
         return AccountString;
     }
 

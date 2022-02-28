8b5178c7e86c35a818eeefed60ced230a5af0753
MahadRauf <91712157+MahadRauf@users.noreply.github.com>
2022-02-27 22:03:09 -0500

    schoolcode and loyalty made instance variables

--- project2/CollegeChecking.java
+++ project2/CollegeChecking.java
@@ -1,46 +1,46 @@
 package project2;
 
 public class CollegeChecking extends Checking{
-	private static int schoolCode;
-	private static final double monthlyInterest = 0.25;
-	private static final double fee = 0.00;
-	private static final int NewBrunswick = 0;
-	private static final int Newark = 1;
-	private static final int Camden = 2;
-	
+    protected int schoolCode;
+    private static final double monthlyInterest = 0.25;
+    private static final double fee = 0.00;
+    private static final int NewBrunswick = 0;
+    private static final int Newark = 1;
+    private static final int Camden = 2;
+
     public CollegeChecking(Profile profile, double balance, int schoolCod) {
         super(profile, balance);
         schoolCode = schoolCod;
-        
+
     }
     public double monthlyInterest() {
-		 return monthlyInterest;
-	 }
-	 
-	 public double fee() {
-		 return fee;
-	 }
-	 
-	 public String getType() {
-		 String accountType = "College Checking";
-		 return accountType;
-	 }
-	 
-	 private String getCampus(int schoolCode) {
-		 if(schoolCode == NewBrunswick) {
-			 return "NEW_BRUNSWICK";
-		 }
-		 if(schoolCode == Newark) {
-			 return "NEWARK";
-		 }
-		 return "CAMDEN";
-	 }
-    
-	 @Override
-		public String toString() {
-			//profile + balance
-			String AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + "::" + getCampus(schoolCode);
-			return AccountString;
-		}
-    
+        return monthlyInterest;
+    }
+
+    public double fee() {
+        return fee;
+    }
+
+    public String getType() {
+        String accountType = "College Checking";
+        return accountType;
+    }
+
+    private String getCampus(int schoolCode) {
+        if(schoolCode == NewBrunswick) {
+            return "NEW_BRUNSWICK";
+        }
+        if(schoolCode == Newark) {
+            return "NEWARK";
+        }
+        return "CAMDEN";
+    }
+
+    @Override
+    public String toString() {
+        //profile + balance
+        String AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + "::" + getCampus(schoolCode);
+        return AccountString;
+    }
+
 }
--- project2/Savings.java
+++ project2/Savings.java
@@ -1,63 +1,63 @@
 package project2;
 
 public class Savings extends Account{
-	public static int loyalty;//0 non-loyal 1 loyal
-	private static final double monthlyInterest = 0.30;
-	private static final double loyalMonthlyInterest = 0.45;
-	private static final double fee = 6.00;
-	private static final double feeWaived = 300.00;
-	private static final double noFee = 0.00;
-	private static final int nonLoyal = 0;
-	private static final int loyal = 1;
-	
-	//for Money Market
-	public Savings(Profile profile, double balance) {
+    protected int loyalty;//0 non-loyal 1 loyal
+    private static final double monthlyInterest = 0.30;
+    private static final double loyalMonthlyInterest = 0.45;
+    private static final double fee = 6.00;
+    private static final double feeWaived = 300.00;
+    private static final double noFee = 0.00;
+    private static final int nonLoyal = 0;
+    private static final int loyal = 1;
+
+    //for Money Market
+    public Savings(Profile profile, double balance) {
         super(profile, balance);
-	       //loyalty = 0;
-	       
+        //this.loyalty = 0;
+
     }
-	
-	public Savings(Profile profile, double balance, int loyalt) {
+
+    public Savings(Profile profile, double balance, int loyalty) {
         super(profile, balance);
-	       loyalty = loyalt;
-	       
+        this.loyalty = loyalty;
+
     }
-	public double monthlyInterest() {
-		if(loyalty == loyal) {
-			return loyalMonthlyInterest;
-		}
-		 return monthlyInterest;
-	 }
-	 
-	 public double fee() {
-		 if(balance >= feeWaived) {
-			 return noFee;
-		 }
-		 return fee;
-	 }
-	 
-	 public String getType() {
-		 String accountType = "Savings";
-		 return accountType;
-	 }
-	 
-	 public String isLoyal() {
-		 if(loyalty == loyal) {
-			 return "::Loyal";
-		 }
-		 return null;
-	 }
-	 @Override
-		public String toString() {
-			//profile + balance
-		 	String AccountString;
-		 	if(loyalty == loyal) {
-		 		AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + isLoyal();
-		 	}
-		 	else {
-		 		AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
-		 	}
-			 
-			return AccountString;
-		}
+    public double monthlyInterest() {
+        if(loyalty == 1) {
+            return loyalMonthlyInterest;
+        }
+        return monthlyInterest;
+    }
+
+    public double fee() {
+        if(balance >= feeWaived) {
+            return noFee;
+        }
+        return fee;
+    }
+
+    public String getType() {
+        String accountType = "Savings";
+        return accountType;
+    }
+
+    public String isLoyal() {
+        if(loyalty == 1) {
+            return "::Loyal";
+        }
+        return null;
+    }
+    @Override
+    public String toString() {
+        //profile + balance
+        String AccountString;
+        if(loyalty == 1) {
+            AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance) + isLoyal();
+        }
+        else {
+            AccountString = getType() + "::" + holder.toString() + "::" + "Balance $" + String.format("%,.2f", balance);
+        }
+
+        return AccountString;
+    }
 }
